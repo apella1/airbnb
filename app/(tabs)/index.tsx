552a6a2637db16da7ebb28198ccb3f31289ad519
apella1 <johncollons5@gmail.com>
2024-05-08 22:44:45 +0300

    feat: set up major routes

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,10 +1,13 @@
 import { View, Text } from "react-native";
 import React from "react";
+import { Link } from "expo-router";
 
 const HomePage = () => {
   return (
     <View>
-      <Text>HomePage</Text>
+      <Link href={"/(modals)/login"}>Login</Link>
+      <Link href={"/(modals)/booking"}>Booking</Link>
+      <Link href={"/listing/32"}>Listing</Link>
     </View>
   );
 };
