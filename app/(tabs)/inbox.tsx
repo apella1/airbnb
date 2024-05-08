844474782ac6b4b45b7cf4dab1ee162d3798d797
apella1 <johncollons5@gmail.com>
2024-05-08 21:19:52 +0300

    feat: add tab bar items

--- app/(tabs)/inbox.tsx
+++ app/(tabs)/inbox.tsx
@@ -1,12 +1,12 @@
 import { View, Text } from "react-native";
 import React from "react";
 
-const HomePage = () => {
+const inbox = () => {
   return (
     <View>
-      <Text>HomePage</Text>
+      <Text>inbox</Text>
     </View>
   );
 };
 
-export default HomePage;
+export default inbox;
